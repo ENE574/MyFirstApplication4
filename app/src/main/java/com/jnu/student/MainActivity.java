110de154ded1867f4789dcb0d82752a7c9580f2d
574lrg <2825119706@qq.com>
2023-11-22 23:34:37 +0800

    11.22:能够实现点击导航栏后页面的切换,但跳转逻辑还有问题

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,19 +15,33 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager;
     private TabLayout tabLayout;
+    private BottomNavigationView bottomNavigationView;
 
-    TaskFragment taskFragments = new TaskFragment();
-    RewardFragment  rewardFragments = new RewardFragment();
-    MineFragment mineFragments =new MineFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                if(item.getItemId()==R.id.navigation_home)
+                    viewPager.setCurrentItem(0);
+                else if(item.getItemId()==R.id.navigation_dashboard)
+                    viewPager.setCurrentItem(3);
+                else if(item.getItemId()==R.id.navigation_notifications)
+                    viewPager.setCurrentItem(4);
+                return true;
+            }
+        });
+//        setContentView(R.layout.fragment_task);
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
-        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(), getLifecycle()));
+
+        viewPager.setAdapter(new PageViewFragmentAdapter(getSupportFragmentManager(), getLifecycle()));
+
         new TabLayoutMediator(tabLayout, viewPager,
                 (tab, position) -> {
                     switch (position) {
@@ -41,44 +54,32 @@ public class MainActivity extends AppCompatActivity {
                         case 2:
                             tab.setText("普通任务");
                             break;
-                        case 3:
-                            tab.setText("副本任务");
-                            break;
                     }
                 }).attach();
     }
-    private void init() {
 
-        //获取页面标签对象
-        viewPager = findViewById(R.id.viewPager);
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                // 页面切换时的处理逻辑
+    private class PageViewFragmentAdapter extends FragmentStateAdapter {
+        public PageViewFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return TaskFragment.newInstance();
+                case 3:
+                    return RewardFragment.newInstance();
+                case 4:
+                    return MineFragment.newInstance();
             }
-        });
-        class PageViewFragmentAdapter extends FragmentStateAdapter {
-            public PageViewFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
-                super(fragmentManager, lifecycle);
-            }
-            @NonNull
-            @Override
-            public Fragment createFragment(int position) {
-                switch(position)
-                {
-                    case 0:
-                        return TaskFragment.newInstance();
-                    case 1:
-                        return  RewardFragment.newInstance();
-                    case 2:
-                        return MineFragment.newInstance();
-                }
-                return TaskFragment.newInstance();
-            }
-            @Override
-            public int getItemCount() {
-                return 3;
-            }
+            return TaskFragment.newInstance();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
         }
     }
 }
